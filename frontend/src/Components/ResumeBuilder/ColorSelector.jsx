630a7daf808d42314e7d75464336d1e71f94401b
DinethSheveen <fdineth4000@gmail.com>
2026-01-10 23:36:08 +0000

    VITE : Resume color selector functionality

---
+++ frontend/src/Components/ResumeBuilder/ColorSelector.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function ColorSelector({setResumeData,resumeColor,setColorMenu,colorMenu}) {
+
+    const [selectedColor, setSelectedColor] = useState(resumeColor)
+    const colors = [
+        { name: "Cyan", value: "#14B8A6" },
+        { name: "Blue", value: "#3B82F6" },
+        { name: "Green", value: "#22C55E" },
+        { name: "Purple", value: "#8B5CF6" },
+        { name: "Pink", value: "#EC4899" },
+        { name: "Red", value: "#EF4444" },
+        { name: "Orange", value: "#F97316" },
+        { name: "Yellow", value: "#EAB308" }
+    ]
+  
+    return (
+        colorMenu &&
+            <div className="absolute top-10 left-0 right-0 bg-gray-200 rounded-md shadow-lg z-20">
+                <hr className="my-3 border border-gray-400" />
+                {
+                    colors.map((color)=>{
+                        return (
+                            <div key={color.value} onClick={()=>{setColorMenu(prev=>!prev); setSelectedColor(color.value); setResumeData(prev=> ({...prev, accent_color : color.value}))}} className={` m-1 p-1 rounded-md ${selectedColor === color.value ? "border-2 border-gray-900":"opacity-50 cursor-pointer"}`} style={{backgroundColor : color.value}}>
+                                <div className="flex justify-between items-center font-bold">
+                                    <p className="my-1 text-white">{color.name}</p>
+                                    {selectedColor === color.value && <p className="text-white">Selected</p>}    
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <hr className="my-3 border border-gray-400" />
+            </div>
+  )
+}
+
+export default ColorSelector

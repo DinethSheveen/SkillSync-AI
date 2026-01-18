87a93e2459f47df98752c705a18552cf09c66475
DinethSheveen <fdineth4000@gmail.com>
2026-01-18 11:35:11 +0000

    EXPRESS : JWT generator function

--- backend/Config/jwt.js
+++ backend/Config/jwt.js
@@ -2,7 +2,7 @@ import jwt  from "jsonwebtoken"
 import "dotenv/config"
 
 const createToken = (id)=>{
-    const token =  jwt.sign({id},process.env.SECRET_KEY)
+    const token =  jwt.sign({id},process.env.SECRET_KEY,{expiresIn : "1d"})
     return token
 }
 

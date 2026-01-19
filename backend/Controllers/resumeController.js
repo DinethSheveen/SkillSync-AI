4380cc978ccffadb502bf3c7b8e763350e0d4de1
DinethSheveen <fdineth4000@gmail.com>
2026-01-19 11:49:19 +0000

    EXPRESS : Resume model and controller functions

---
+++ backend/Controllers/resumeController.js
@@ -0,0 +1,90 @@
+import { resumeModel } from "../Models/resumeModel.js"
+import { userModel } from "../Models/userModel.js"
+
+// CREATE A RESUME
+export const createResume = async(req,res)=>{
+    try {
+        const userId = req.userId
+
+        // RESUME TITLE
+        const {title} = req.body
+
+        if(!title){
+            return res.status(400).json({message : "Resume Title is Required"})
+        }
+
+        // CREATING THE NEW RESUME
+        const newResume = await resumeModel.create({title,user : userId})
+
+        // ADDING THE NEW RESUME TO THE USER'S RESUME LIST
+        const updatedUser = await userModel.findByIdAndUpdate(userId,{$push : {resumes : newResume._id}})
+
+        if(!updatedUser){
+            return res.status(404).json({message : "Invalid user id"})
+        }
+
+        res.status(201).json({message : "Resume created successfully.",newResume})
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+}
+
+// RETRIEVE RESUME 
+export const getResume = async(req,res)=>{
+    try {
+        const userId = req.userId
+
+        const resumes = await resumeModel.find({user : userId})
+
+        if(!resumes){
+            return res.status(400).json({message : "No resumes created yet..."})
+        }
+
+        res.status(200).json({resumes})
+
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+}
+
+// UPDATE RESUME
+export const updateResume = async(req,res)=>{
+    try {
+        const userId = req.userId
+        const {resumeId} = req.params
+        const {resumeData} = req.body
+
+        if(!resumeId){
+            return res.status(404).json({message : "Invalid resume id"})
+        }
+
+        const resume = await resumeModel.findByIdAndUpdate(resumeId,{resumeData},{new:true})
+
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+}
+
+// DELETE RESUME
+export const deleteResume = async(req,res)=>{
+    try {
+        const userId = req.userId
+
+        const {resumeId} = req.params
+
+        // DELETING THE RESUME ID FROM THE RESUME MODEL
+        const resume = await resumeModel.deleteById(resumeId)
+
+        // REMOVING THE RESUME FROM THE USER'S RESUMES 
+        const updatedUser = await userModel.findByIdAndUpdate(userId,{$pull : {resumes : resumeId} })
+        
+        if(!updatedUser){
+            return res.status(404).json({message : "Invalid user id"})
+        }
+
+        res.status(200).json({message : "Resume deleted successfully"})
+
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+}
---
+++ backend/Models/resumeModel.js
@@ -0,0 +1,68 @@
+import mongoose, { Types } from "mongoose";
+
+const resumeSchema = mongoose.Schema({
+    user : {
+        type : Types.ObjectId,
+        ref  : "user",
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    template : {
+        type : String,
+        default : "classic"
+    },
+    color : {
+        type : String,
+        default : "14B8A6"
+    },
+    public : {
+        type : Boolean,
+        default : false
+    },
+    personalInfo : {
+        fullName : {type : String},
+        email : {type : String},
+        phone : {type : String},
+        location : {type : String},
+        linkedin : {type : String},
+        github : {type : String},
+        website : {type : String},
+        profession : {type : String},
+        image : {type : String}
+    },
+    personalSummary : {
+        type : String
+    },
+    skills : [{type : String}],
+    experience : [
+        {
+            company : {type : String},
+            position : {type : String},
+            start_date : {type : String},
+            end_date : {type : String},
+            description : {type : String},
+            isCurrent : {type : Boolean,default : false}
+        }
+    ],
+    education : [
+        {
+            institution : {type : String},
+            degree : {type : String},
+            field : {type : String},
+            graduationDate : {type : String},
+            gpa : {type : String}
+        }
+    ],
+    projects : [
+        {
+            name : {type : String},
+            type : {type : String},
+            description : {type : String},
+        }
+    ],
+},{timestamps:true})
+
+export const resumeModel = mongoose.model("resume",resumeSchema)

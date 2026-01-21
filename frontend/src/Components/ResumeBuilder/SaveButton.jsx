374e7f9fa45ac4ed89fdeb7d7ea0d41f0bd75725
DinethSheveen <fdineth4000@gmail.com>
2026-01-21 13:59:51 +0000

    VITE : Save button component to save resume details

---
+++ frontend/src/Components/ResumeBuilder/SaveButton.jsx
@@ -0,0 +1,31 @@
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
+function SaveButton({resumeData}) {
+
+  const {resumeId} = useParams()     
+
+  const saveResumeData = async(e) =>{
+    e.preventDefault()
+    try {
+
+      const response = await axios.put(`http://localhost:3000/api/resumes/update/${resumeId}`,resumeData,{headers : {Authorization : localStorage.getItem("token")}})
+      console.log(response);
+      console.log(resumeData);
+      
+    } catch (error) {
+      console.log(error?.response);
+    }
+  }
+
+  return (
+    // SAVE BUTTON 
+    <div onClick={saveResumeData} className="my-2">
+        <button className="border border-violet-500 text-violet-800 bg-violet-300 cursor-pointer py-2 px-4 w-fit rounded-lg hover:bg-violet-400 active:bg-violet-500">Save Changes</button>
+    </div>
+  )
+}
+
+export default SaveButton
+
+

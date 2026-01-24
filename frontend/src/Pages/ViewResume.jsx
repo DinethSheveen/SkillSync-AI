8881ca5f8147525eb515a4dfabc04d90bdd286e0
DinethSheveen <fdineth4000@gmail.com>
2026-01-24 22:12:51 +0000

    VITE : Fetched resumeData and displayed the resume with the respective data

--- frontend/src/Pages/ViewResume.jsx
+++ frontend/src/Pages/ViewResume.jsx
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react"
 import Preview from "../Components/ResumeBuilder/Preview"
 import { useParams } from "react-router-dom"
-import { dummyResumeData } from "../assets/assets"
+import axios from "axios"
 
 function ViewResume() {
 
   const {resumeId} = useParams()
-  const [resume, setResume] = useState(null)
-
-  console.log(resume);
-  
+  const [resume, setResume] = useState(null)  
 
   useEffect(()=>{
-    const fetchResumeData = ()=>{
-      setResume(dummyResumeData.find((resume)=>{return resume._id === resumeId}) || null)
+    const fetchResumeData = async()=>{
+      const response = await axios.get(`http://localhost:3000/api/resumes/get-by-id/${resumeId}`,{headers : {Authorization : localStorage.getItem("token")}})
+      
+      setResume(response?.data?.resume?.resumeData)
+      document.title = response?.data?.resume?.resumeTitle
     }
     fetchResumeData()
   },[resumeId])
 
   return (
-    <div className="max-w-[50%] py-5 mx-auto">
-      <Preview data={resume} template={resume?.template} accent_color={resume?.accent_color}/>
+    <div className="max-w-[60%] py-5 mx-auto">
+      <Preview data={resume} template={resume?.template} color={resume?.color}/>
     </div>
   )
 }

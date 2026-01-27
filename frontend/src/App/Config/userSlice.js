6e31f78bdbf5bd0293b5ca2901de5157a7c3303f
DinethSheveen <fdineth4000@gmail.com>
2026-01-27 17:15:17 +0000

    VITE : authInit function on redux

--- frontend/src/App/Config/userSlice.js
+++ frontend/src/App/Config/userSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+    isAuthInit : false,
     isAuthenticated : false,
     token : null,
     user : null
@@ -11,21 +12,26 @@ const userSlice = createSlice({
     initialState,
     reducers : {
         login : (state,action)=>{
-            state.isAuthenticated = true,
-            state.token = action.payload.token,
+            state.isAuthInit = true
+            state.isAuthenticated = true
+            state.token = action.payload.token
             state.user = action.payload.user
         },
         logout : (state)=>{
-            state.isAuthenticated = false,
-            state.token = null,
+            state.isAuthInit = false
+            state.isAuthenticated = false
+            state.token = null
             state.user = null 
         },
         setUser : (state,action)=>{
-            state.user = action.payload 
+            state.user = action.payload
+        },
+        initAuth : (state)=>{
+            state.isAuthInit = true
         }
     }
 })
 
 
-export const {login,logout,setUser} = userSlice.actions
+export const {login,logout,setUser,initAuth} = userSlice.actions
 export default userSlice.reducer

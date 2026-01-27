e39050f0009f8fd9eea4ec24289dc40c985e086e
DinethSheveen <fdineth4000@gmail.com>
2026-01-27 13:12:05 +0000

    VITE : Getting user information from redux and displaying the name

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,36 +1,15 @@
 import { Link, useNavigate } from "react-router-dom"
 import logo from "/logo.png"
-import { useDispatch, useSelector } from "react-redux"
-import axios from "axios";
-import { useEffect } from "react";
-import { setUser } from "../App/Config/userSlice";
+import { useSelector } from "react-redux"
 
 function Navbar() {
-
     const {user} = useSelector(state => state.user)
-    const dispatch = useDispatch()
-    console.log(user);
-    
-    const getUser = async()=>{
-        try {
-            const response = await axios.get("http://localhost:3000/api/auth/authenticate",{headers : {Authorization : localStorage.getItem("token")}})
-            console.log(response);
-            dispatch(setUser(response?.data?.userInfo))
-        } catch (error) {
-            console.log(error);   
-        }
-    }
-    
 
     const navigate = useNavigate()
     const logout = ()=>{
         localStorage.removeItem("token")
         navigate("/")
     }
-
-    useEffect(()=>{
-        getUser()
-    },[])
 
   return (
     <div className="navbar print:hidden">
@@ -40,7 +19,7 @@ function Navbar() {
                     <img src={logo} alt="logo" className="h-5 w-20"/>
                 </Link>
                 <div className="flex gap-2 items-center">
-                    <p>Hello {user?.name}!</p>
+                    <p>Hello {user?.name}</p>
                     <button onClick={logout} className="border border-violet-600 px-2 py-1 rounded-[5px] text-violet-700 hover:text-white hover:bg-violet-700 transition-colors">Log Out</button>
                 </div>
             </div>

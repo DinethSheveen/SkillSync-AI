a408b4a5f5ac8a93d24a0c7e8e1bf549779bff71
DinethSheveen <fdineth4000@gmail.com>
2026-01-27 13:06:13 +0000

    VITE : Resume templates

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import logo from "/logo.png"
+import { useDispatch, useSelector } from "react-redux"
+import axios from "axios";
+import { useEffect } from "react";
+import { setUser } from "../App/Config/userSlice";
 
 function Navbar() {
+
+    const {user} = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    console.log(user);
+    
+    const getUser = async()=>{
+        try {
+            const response = await axios.get("http://localhost:3000/api/auth/authenticate",{headers : {Authorization : localStorage.getItem("token")}})
+            console.log(response);
+            dispatch(setUser(response?.data?.userInfo))
+        } catch (error) {
+            console.log(error);   
+        }
+    }
+    
+
+    const navigate = useNavigate()
+    const logout = ()=>{
+        localStorage.removeItem("token")
+        navigate("/")
+    }
+
+    useEffect(()=>{
+        getUser()
+    },[])
+
   return (
     <div className="navbar print:hidden">
         <div className="fixed top-0 w-full py-2 px-4 bg-violet-300 z-20">
@@ -10,8 +40,8 @@ function Navbar() {
                     <img src={logo} alt="logo" className="h-5 w-20"/>
                 </Link>
                 <div className="flex gap-2 items-center">
-                    <p>Hi Dineth!</p>
-                    <Link to="/" className="border border-violet-600 px-2 py-1 rounded-[5px] text-violet-700 hover:text-white hover:bg-violet-700 transition-colors">Log Out</Link>
+                    <p>Hello {user?.name}!</p>
+                    <button onClick={logout} className="border border-violet-600 px-2 py-1 rounded-[5px] text-violet-700 hover:text-white hover:bg-violet-700 transition-colors">Log Out</button>
                 </div>
             </div>
         </div>

99dbfc7bfeb4caad35ac45abbedda7bd45881a8d
DinethSheveen <fdineth4000@gmail.com>
2026-02-03 13:26:49 +0000

    EXPRESS : Resume upload router

--- backend/Routes/aiRouter.js
+++ backend/Routes/aiRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { enhanceJobDescription, enhanceSummary } from "../Controllers/aiController.js";
+import { enhanceJobDescription, enhanceSummary, uploadResume } from "../Controllers/aiController.js";
 import getMe from "../Middelware/authMiddleware.js";
 
 const aiRouter = Router();
 
 aiRouter.post("/summary",getMe,enhanceSummary)
 aiRouter.post("/job-description",getMe,enhanceJobDescription)
+aiRouter.post("/upload-resume",getMe,uploadResume)
 
 export default aiRouter;
